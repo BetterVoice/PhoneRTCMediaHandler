38eb726a505aa8675277c5940c865c25c9956910
Thomas Quintana <thomas@bettervoice.com>
2014-11-07 19:34:28 -0800

    Debugging.

--- index.js
+++ index.js
@@ -126,17 +126,17 @@ module.exports = function(SIP) {
     		}
   		};
 
-      window.console.log(SIP.Utils.Q);
-      // return new SIP.Utils.Promise(function (resolve, reject) {
-      //   this.phonertc.session = new cordova.plugins.phonertc.Session(config);
-      //   this.phonertc.session.on('sendMessage', function (data) {
-      //     if(data.type === 'offer' ||
-      //        data.type === 'answer') {
-      //       resolve(data.sdp);
-      //     }
-      //   });
-      //   this.phonertc.session.call();
-      // });
+      window.console.log(globals.Promise);
+      return new SIP.Utils.Promise(function (resolve, reject) {
+        this.phonertc.session = new cordova.plugins.phonertc.Session(config);
+        this.phonertc.session.on('sendMessage', function (data) {
+          if(data.type === 'offer' ||
+             data.type === 'answer') {
+            resolve(data.sdp);
+          }
+        });
+        this.phonertc.session.call();
+      });
   	}}
 	});
 
