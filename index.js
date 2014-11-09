1802221d305b104b962e8f72b488c3f524fae59d
Thomas Quintana <thomas@bettervoice.com>
2014-11-09 08:22:06 -0800

    Debugging inbound calls

--- index.js
+++ index.js
@@ -85,6 +85,7 @@ module.exports = function(SIP) {
   		if(isNewCall) {
         this.startSession(false);
       }
+      window.console.log(sdp);
   		var session = this.phonertc.session;
   		if(phonertc.role === 'caller') {
   			session.receiveMessage({'type': 'answer', 'sdp': sdp});
@@ -93,6 +94,7 @@ module.exports = function(SIP) {
   		}
   		this.phonertc.state = 'connected';
       onSuccess();
+      window.console.log('End of setDescription()');
   	}},
 
   	isMuted: {writable: true, value: function isMuted() {
