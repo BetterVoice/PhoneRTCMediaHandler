782b105ed0ab58971fe257e67b255bfa391db2d2
Thomas Quintana <thomas@bettervoice.com>
2014-12-18 10:12:35 -0800

    ditto

--- index.js
+++ index.js
@@ -169,17 +169,6 @@ module.exports = function(SIP) {
           if(data.type === 'answer') {
             if(onSuccess) { onSuccess(); }
           }
-          // Store DTLS session credentials since we can't renegotiate DTLS later.
-          var fingerprint = null, ufrag = null, pwd = null;
-          var lines = data.sdp.split('\r\n');
-          for(var index = 0; index < lines.length; index++) {
-            if(lines[index].match(/a=fingerprint:.*/g)) fingerprint = lines[index];
-            if(lines[index].match(/a=ice-ufrag:.*/g)) ufrag = lines[index];
-            if(lines[index].match(/a=ice-pwd:.*/g)) pwd = lines[index];
-          }
-          phonertc.iceRemoteFingerprint = fingerprint;
-          phonertc.iceRemoteUfrag = ufrag;
-          phonertc.iceRemotePwd = pwd;
         } else if(data.type === 'candidate') {
           // If we receive another candidate we stop
           // the watchdog and restart it again later.
@@ -224,10 +213,9 @@ module.exports = function(SIP) {
           } else if(phonertc.state === 'muted') {
             phonertc.sdp = phonertc.sdp.replace(/a=sendrecv\r\n/g, 'a=recvonly\r\n');
           }
-          // Use original DTLS credentials.
-          phonertc.sdp = phonertc.sdp.replace(/a=fingerprint:.*\r\n/g, phonertc.iceRemoteFingerprint + '\r\n');
-          phonertc.sdp = phonertc.sdp.replace(/a=ice-ufrag:.*\r\n/g, phonertc.iceRemoteUfrag + '\r\n');
-          phonertc.sdp = phonertc.sdp.replace(/a=ice-pwd:.*\r\n/g, phonertc.iceRemotePwd + '\r\n');
+          // PhoneRTC blows up if we try to renegotiate the DTLS settings.
+          phonertc.sdp = phonertc.sdp.replace(/a=ice-ufrag:.*\r\n/g, '');
+          phonertc.sdp = phonertc.sdp.replace(/a=ice-pwd:.*\r\n/g, '');
           // Wrap up.
           if(onSuccess) { onSuccess(phonertc.sdp); }
           window.console.log('\n\n' + phonertc.sdp + '\n\n');
