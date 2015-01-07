dfc551d0ebaff301ffa1d674382e00ce5ce9622b
Thomas Quintana <thomas@bettervoice.com>
2015-01-06 20:21:22 -0800

    Working on hold.

--- index.js
+++ index.js
@@ -90,11 +90,7 @@ module.exports = function(SIP) {
         this.startSession(sdp, onSuccess, onFailure);
       }
   		var session = phonertc.session;
-  		if((phonertc.role === 'caller' &&
-          phonertc.state === 'disconnected') ||
-         phonertc.state === 'connected' ||
-         phonertc.state === 'holding' ||
-         phonertc.state === 'muted') {
+  		if(phonertc.role === 'caller' && phonertc.state === 'disconnected') {
         session.receiveMessage({'type': 'answer', 'sdp': sdp});
         onSuccess();
         if(phonertc.state === 'disconnected') {
