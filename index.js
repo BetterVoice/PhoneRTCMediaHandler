169aa582c6542feedb35a4f104f662bb959c80cb
Thomas Quintana <thomas@bettervoice.com>
2015-01-08 05:09:07 -0800

    Updated to reflect changes to phonertc plugin.

--- index.js
+++ index.js
@@ -16,27 +16,6 @@ module.exports = function(SIP) {
 		// Create a logger.
   	window.console.log('Loading the PhoneRTC 2.0 Media Handler.');
 
-  	// Try to use a Turn server provided by sip.js.
-  	var turnServers = [];
-  	if(options) {
-  		turnServers = options.turnServers;
-  	} else {
-  		turnServers = session.ua.configuration.turnServers;
-  	}
-  	if(turnServers && turnServers.length > 0) {
-  		this.turnServer = {
-  			'host': turnServers[0].urls,
-  	    'username': turnServers[0].username,
-  	    'password': turnServers[0].password
-  		};
-  	} else {
-  		this.turnServer = {
-  			'host': 'turn:54.191.155.251',
-        'username': 'bettervoice',
-        'password': 'B3tt3rV01c3'
-  		};
-  	}
-
   	// Finish initialization.
   	this.phonertc = {
   		/*
@@ -75,43 +54,11 @@ module.exports = function(SIP) {
   		if(isInitiator && phonertc.state === 'disconnected') {
         this.startSession(null, onSuccess, onFailure);
       } else {
-        /*
-        if(phonertc.state === 'holding' || phonertc.state == 'connected') {
-          var watchdog = null;
-          phonertc.session.on('sendMessage', function (data) {
-            if(data.type === 'offer') {
-              phonertc.sdp = data.sdp;
-            } else if(data.type === 'candidate') {
-              if(watchdog !== null) {
-                clearTimeout(watchdog);
-              }
-              // Append the candidate to the SDP.
-              var candidate = "a=" + data.candidate + "\r\n";
-              if(data.id === 'audio') {
-                phonertc.sdp += candidate;
-              }
-              // Start the watchdog.
-              watchdog = setTimeout(function() {
-                if(phonertc.state == 'holding') {
-                  onSuccess(phonertc.sdp.replace(/a=sendrecv\r\n/g, 'a=sendonly\r\n'));
-                } else {
-                  onSuccess(phonertc.sdp.replace(/a=sendrecv\r\n/g, ''));
-                }
-              }, 500);
-            }
-          });
-          phonertc.session.renegotiate();
-          */
-          if(phonertc.state === 'holding') {
-            onSuccess(phonertc.sdp.replace(/a=sendrecv\r\n/g, 'a=sendonly\r\n'));
-          } else {
-            onSuccess(phonertc.sdp.replace(/a=sendrecv\r\n/g, ''));
-          }
-        /*
+        if(phonertc.state === 'holding') {
+          onSuccess(phonertc.sdp.replace(/a=sendrecv\r\n/g, 'a=sendonly\r\n'));
         } else {
-          onSuccess(phonertc.sdp);
+          onSuccess(phonertc.sdp.replace(/a=sendrecv\r\n/g, ''));
         }
-        */
       }
   	}},
 
@@ -123,7 +70,7 @@ module.exports = function(SIP) {
       }
   		var session = phonertc.session;
   		if((phonertc.role === 'caller' && phonertc.state === 'disconnected')) {
-        session.receiveMessage({'type': 'answer', 'sdp': sdp});
+        session.receive({'type': 'answer', 'sdp': sdp});
         onSuccess();
         if(phonertc.state === 'disconnected') {
           phonertc.state = 'connected';
@@ -180,20 +127,14 @@ module.exports = function(SIP) {
   	startSession: {writable: true, value: function startSession(sdp, onSuccess, onFailure) {
       var phonertc = this.phonertc;
   		phonertc.role = sdp === null ? 'caller' : 'callee';
-  		var config = {
-  			isInitiator: phonertc.role === 'caller',
-    		turn: this.turnServer,
-    		streams: {
-    			audio: true,
-    			video: false
-    		}
-  		};
       // Unfortunately, there is no message to let us know
       // that PhoneRTC has finished gathering ice candidates.
       // We use a watchdog to make sure all the ICE candidates
       // are allocated before returning the SDP.
       var watchdog = null;
-      phonertc.session = new cordova.plugins.phonertc.Session(config);
+      phonertc.session = new cordova.plugins.phonertc.Session({
+        isInitiator: phonertc.role === 'caller'
+      });
       phonertc.session.on('sendMessage', function (data) {
         if(data.type === 'offer' || data.type === 'answer') {
           phonertc.sdp = data.sdp;
@@ -226,10 +167,10 @@ module.exports = function(SIP) {
       // If we received a session description pass it on to the
       // PhoneRTC plugin.
       if(phonertc.role === 'callee') {
-        phonertc.session.receiveMessage({'type': 'offer', 'sdp': sdp});
+        phonertc.session.receive({'type': 'offer', 'sdp': sdp});
       }
       // Start the media.
-      phonertc.session.call();
+      phonertc.session.initialize();
   	}}
 	});
 
